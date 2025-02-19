6bdeddcf9524007dcee16e57520fcfce1581ed86
beshoycoding123 <beshoy.shawky.2013@gmail.com>
2025-02-19 17:32:41 +0400

    checking divisibility

---
+++ checking divisability.py
@@ -0,0 +1,6 @@
+numerator=int(input("can you enter a numerator value: "))
+denomenator=int(input("can you enter a denomenator value: "))
+if numerator%denomenator==0:
+    print("the numerator is divisible by the denomenator")
+else:
+    print("the numerator is not divisible by the denomenator")
